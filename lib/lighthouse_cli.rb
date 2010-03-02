aefd7bbded2f5c42ed054c34aca80a38f0a383cf
Kevin McFadden <kevin@conceptsahead.com>
2010-03-03 00:53:29 +0800

    support a global config file unless a local Lhcfile exists.

--- lib/lighthouse_cli.rb
+++ lib/lighthouse_cli.rb
@@ -9,24 +9,28 @@ require 'lighthouse_cli/commands'
 require 'lighthouse_cli/parser'
 
 module LighthouseCLI
-  DEFAULT_CONFIG_PATH = "Lhcfile"
-  
+  if (File.exists?("Lhcfile"))
+    DEFAULT_CONFIG_PATH = "Lhcfile"
+  else
+    DEFAULT_CONFIG_PATH = File.join(ENV['HOME'], ".lighthouse_cli", "Lhcfile")
+  end
+
   def bootstrap!
     Authenticator.authenticate!
     ::Lighthouse.account = Config.account
   end
-  
+
   def project
     project_name = Config.project
-    
+
     @project ||= if project_name
       ::Lighthouse::Project.find(:all).find{|p| p.name == Config.project}
     else
       nil
     end
-    
+
     @project || (raise RuntimeError.new("Project not found or wasn't configured."))
   end
-  
+
   module_function :bootstrap!, :project
 end
